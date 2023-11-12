20dd9bfb385645ef967ef9014c6af5d0b79f516a
Nima <87418027+nimaprgrmr@users.noreply.github.com>
2023-11-12 11:25:41 +0330

    Add files via upload

--- app.py
+++ app.py
@@ -7,15 +7,15 @@ import plotly.express as px
 from data_preprocessing import read_data, make_score
 from fastapi import FastAPI
 
-
 path = '2021_sales.csv'
 # Sample data
 df = read_data(path)
 new_df = make_score(df)
 
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 # Create a Dash app
-dash_app = dash.Dash(__name__, url_base_pathname="/sale-analyse/")
+dash_app = dash.Dash(__name__, url_base_pathname="/sale-analyse/", external_stylesheets=external_stylesheets)
 
 # different values of font-family styles = ['Arial, sans-serif', 'Helvetica, Arial', 'sans-serif', 'Georgia, serif',
 #                       'Palatino, serif', 'Times New Roman, serif, 'Courier New, monospace', 'Cursive' or 'fantasy'
@@ -31,18 +31,44 @@ values = [range(1, 23)]
 
 # Define the layout of the app
 dash_app.layout = html.Div([
-    html.H1("Asrejadid sales analysis", style=custom_font_style),
+    html.Div([
+        html.H4(f"Asre jadid sales analysis"),
+        # Help Button
+        html.Button('Help', id='help-button', n_clicks=0,
+                    style={'margin-left': '1100px', 'background-color': 'rgb(13,152,186)', 'color': 'white'}),
+    ], style={'display': 'flex', 'align-items': 'center'}),
 
     # Dropdown for selecting a column
-    dcc.Dropdown(
-        id='dropdown-column',
-        options=[{'label': col, 'value': col} for col in new_df.columns],
-        value=new_df.columns[0]  # Set the default value here
+    html.Div([
+        dcc.Dropdown(
+            id='dropdown-column',
+            options=[{'label': col, 'value': col} for col in new_df.columns],
+            value=new_df.columns[0],  # Set the default value here
+            style={'background-color': 'rgb(230,240,245)'}
+        ), ]
     ),
 
-    # Plot to display the selected column
-    dcc.Graph(id='bar-plot'),
-    dcc.Graph(id='bar-plot1')  # We can add so many plots but with different `id`
+    html.Div([
+        dcc.Graph(id='bar-plot'), ], style={'margin-top': '25px'}),
+    # dcc.Graph(id='bar-plot1')  # We can add so many plots but with different `id`
+
+    html.Div(id='help-modal', style={'display': 'none'}, children=[
+        html.Div([
+            html.H4("Dashboard Guide"),
+            dcc.Markdown("""
+                    This is a guide for using your dashboard.
+
+                    1. Use the dropdowns to select the desired category.
+                    2. The y-axis values are between 0 to 100, representing the score of each branch in the category.
+                    3. Click on the "Help" button to close this guide.
+
+                    Enjoy using the dashboard!
+                """, style={'color': 'white', 'background-color': 'rgba(13,152,186, 0.7)', 'padding': '10px', 'border-radius': '10px'}),
+        ], style={'position': 'fixed', 'top': '15%', 'left': '55%', 'transform': 'translate(-26%, -50%)'}),
+        html.Div(id='modal-background',
+                 style={'position': 'fixed', 'top': 50, 'left': 50, 'width': '100%', 'height': '50%',
+                        'background-color': 'rgba(0, 0, 0, 0.5)', 'display': 'none'}),
+    ]),
 ])
 
 
@@ -56,23 +82,34 @@ dash_app.layout = html.Div([
 #                              px.colors.sequential.Plasma, px.colors.sequential.Magma, px.colors.sequential.CubeYF]
 def update_plot(selected_column):
     fig = px.bar(new_df, x=new_df.index, y=selected_column, color=selected_column,
-                 color_continuous_scale=px.colors.sequential.Inferno)
+                 color_continuous_scale=px.colors.sequential.Viridis)
+    fig.update_traces(hovertemplate='%{y:.3f}')
     fig.update_layout(height=600)  # Set height of plot and the width is default value
-    tick_label_font_size = 16      # This code set number of size of x labels and x values
+    tick_label_font_size = 16  # This code set number of size of x labels and x values
     # Set xlabel and their configuration
-    fig.update_xaxes(title_text="<b>Branches</b>", title_font=dict(size=18), tickfont=dict(size=tick_label_font_size))
+    fig.update_xaxes(title_text="Branches", title_font=dict(size=18), tickfont=dict(size=tick_label_font_size))
     # Set ylabel and their configuration We don't change the size of y values
-    fig.update_yaxes(title_text="<b>Branches score between 0-100</b>", title_font=dict(size=18))
+    fig.update_yaxes(title_text="Branches score between 0-100", title_font=dict(size=18))
     # Set the title for plot
-    fig.update_layout(title=f'<i>distribution of sales in {selected_column} category of each branch</i>',
-                      title_font=dict(family='Helvetica, Arial, sans-serif', size=20, color='gray'))
+    fig.update_layout(title=f'distribution of sales in "{selected_column}" category for each branch',
+                      )
     return fig
+
+
+@dash_app.callback(
+    Output('help-modal', 'style'),
+    Output('modal-background', 'style'),
+    Input('help-button', 'n_clicks'),
+    prevent_initial_call=True
+)
+def toggle_modal(help_clicks):
+    if help_clicks is None:
+        help_clicks = 0
+    if help_clicks % 2 == 1:  # Odd number of clicks on Help or Close button
+        return {'display': 'block'}, {'display': 'block'}
+    else:
+        return {'display': 'none'}, {'display': 'none'}
 
 
 if __name__ == '__main__':
     dash_app.run_server(debug=True)
-
-
-# if __name__ == '__main__':
-#     import uvicorn
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
